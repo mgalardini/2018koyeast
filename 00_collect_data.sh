3d6f6ce7994ec036068bcc6bbd8aa23d4991bbf2
Marco Galardini <marco@ebi.ac.uk>
2018-08-30 16:40:15 +0100

    Remove unused script

--- 00_collect_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp ~/leia/2017yeast/20170928/emap/all_genes/ko_scores.tsv data/ko_scores.tsv
-wget -O data/conditions.tsv "https://docs.google.com/spreadsheets/d/14i_5lWftFKMTwS4dOWULescrMCizwndyP-k6Y-P_SoI/export?gid=54998998&format=tsv"
